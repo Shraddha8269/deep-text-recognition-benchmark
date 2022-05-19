626d4059a041a0fe5dcf53303b91b4edaba11adb
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-05-19 14:28:16 +0530

    Update visformer.py

--- modules/visformer.py
+++ modules/visformer.py
@@ -58,20 +58,7 @@ class ViTSTR(VisionTransformer):
         self.num_classes = num_classes
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
-    def forward_features(self, x):
-        B = x.shape[0]
-        x = self.patch_embed(x)
-
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
-
-        for blk in self.blocks:
-            x = blk(x)
-
-        x = self.norm(x)
-        return x
+ 
 
     def forward(self, x, seqlen=25):
         x = self.forward_features(x)
